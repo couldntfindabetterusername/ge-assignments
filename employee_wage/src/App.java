c0d05542f9381f5f6d538d45fdabda7c68f43f1e
Sujal Barsaiyan <sujal.barsaiyan1234@gmail.com>
2023-12-20 00:01:49 +0530

    checking the attendance

--- employee_wage/src/App.java
+++ employee_wage/src/App.java
@@ -1,5 +1,13 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Welcome to employee wage builder");
+
+        int attendance = Math.random() > 0.5 ? 1 : 0;
+
+        if(attendance == 1){
+            System.out.println("Employee is present");
+        }else{
+            System.out.println("Employee is absent");
+        }
     }
 }
