2705e641fd66bad957a79cb57b3f136d80461b28
Sujal Barsaiyan <sujal.barsaiyan1234@gmail.com>
2023-12-19 15:51:10 +0530

    Length of the line is calculated

--- line_comparision/src/App.java
+++ line_comparision/src/App.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Welcome to line comparision computation");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter start point of line: ");
+        int x1 = scanner.nextInt();
+        int y1 = scanner.nextInt();
+
+        System.out.print("Enter end point of line: ");
+        int x2 = scanner.nextInt();
+        int y2 = scanner.nextInt();
+
+        double length = Math.sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
+
+        System.out.println("Length of the line is: " + length);
+
+        scanner.close();
     }
 }
