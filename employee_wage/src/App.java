89aca93ae5f0e66a48d0cf40011b4a6ab73d23fc
Sujal Barsaiyan <sujal.barsaiyan1234@gmail.com>
2023-12-20 00:38:57 +0530

    checking if the employee is full time

--- employee_wage/src/App.java
+++ employee_wage/src/App.java
@@ -11,11 +11,22 @@ public class App {
             return;
         }
 
+        int isFullTime = Math.random() > 0.5 ? 1 : 0;
+        
         int WAGE_PER_HOUR = 20;
-        int EMP_HRS = 8;
+        int EMP_HRS = 0;
+
+        if(isFullTime == 1) {
+            System.out.println("Employee is fulltime");
+            EMP_HRS = 8;
+        }
+        else {
+            System.out.println("Employee is parttime");
+            EMP_HRS = 4;
+        }
 
         int empWage = EMP_HRS * WAGE_PER_HOUR;
 
-        System.out.println(empWage);
+        System.out.println("Wage of the employee is: " + empWage);
     }
 }
