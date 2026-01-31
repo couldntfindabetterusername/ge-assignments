2a95871e62d6eeec92786a398b812951c8b0c535
Sujal Barsaiyan <sujal.barsaiyan1234@gmail.com>
2026-01-31 22:04:51 +0530

    test commit

--- classwork/Pattern1.java
+++ classwork/Pattern1.java
@@ -4,6 +4,7 @@ class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Test commit");
         System.out.print("Enter an integer n: ");
         int n = scanner.nextInt();
 
